4044feb5d5bd61132c3208fc15bc0935f77fa5d3
Pedro Libradilla <plibradilla@hotmail.com>
2023-09-11 17:14:06 +0800

    Create VPC module

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,8 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = var.instance_tenancy
+# Basic VPC Module
+resource "aws_vpc" "this" {
+
+  vpc_name             = var.vpc_name
+  environment          = var.environment
+  cidr_block           = var.cidr_block
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
 
   tags = {
-    Name = "main"
+    Name = "${var.name}-${var.environment}"
   }
 }
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -1,9 +1,24 @@
+variable "vpc_name" {
+  type    = string
+  default = "custom_vpc"
+
+}
 variable "cidr_block" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "instance_tenancy" {
-  type = string
+  type    = string
   default = "default"
+}
+
+variable "enable_dns_hostnames" {
+  type    = bool
+  default = true
+}
+
+variable "enable_dns_support" {
+  type    = bool
+  default = true
 }
