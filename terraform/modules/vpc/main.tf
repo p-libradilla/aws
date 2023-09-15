13bbec2f45e260bef3d8e36a4c2395ddd5575dcc
EC2 Default User <ec2-user@ip-172-31-15-67.ec2.internal>
2023-09-15 06:23:26 +0000

    Update subnet cidr name

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "this" {
   vpc_id     = aws_vpc.this.id
-  cidr_block = var.cidr_block
+  cidr_block = var.subnet_cidr_block
 
   tags = {
     Name = var.subnet_name
