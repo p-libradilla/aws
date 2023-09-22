4fc67c2df8eb1b60a156da10d51aba18866a2b31
EC2 Default User <ec2-user@ip-172-31-15-67.ec2.internal>
2023-09-22 03:41:47 +0000

    Updates

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,8 +1,6 @@
 # Basic VPC Module ####
 resource "aws_vpc" "this" {
 
-  vpc_name             = var.vpc_name
-  environment          = var.environment
   cidr_block           = var.cidr_block
   instance_tenancy     = var.instance_tenancy
   enable_dns_support   = var.enable_dns_support
